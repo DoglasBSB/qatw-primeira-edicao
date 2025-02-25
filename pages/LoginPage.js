ba353c5a9bc04d4c0d79c6ee889cf343577cf530
DoglasBSB <doglas_bsb@hotmail.com>
2025-02-25 16:39:10 -0300

    adjusting the pipeline to work with the playwright docker image

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,27 +1,27 @@
 export class LoginPage {
+
   constructor(page) {
-    this.page = page;
+      this.page = page
   }
 
   async acessaPagina() {
-    await this.page.goto("http://paybank-mf-auth:3000/");
+      await this.page.goto('http://paybank-mf-auth:3000/');
   }
 
-  async informarCpf(cpf) {
-    await this.page.getByRole("textbox", { name: "Digite seu CPF" }).fill(cpf);
-    await this.page.getByRole("button", { name: "Continuar" }).click();
+  async informaCpf(cpf) {
+      await this.page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(cpf);
+      await this.page.getByRole('button', { name: 'Continuar' }).click();
   }
 
-  async informarSenha(senha) {
-    for (const digito of senha) {
-      await this.page.getByRole("button", { name: digito }).click();
-    }
-    await this.page.getByRole("button", { name: "Continuar" }).click();
+  async informaSenha(senha) {
+      for (const digito of senha) {
+          await this.page.getByRole('button', { name: digito }).click();
+      }
+      await this.page.getByRole('button', { name: 'Continuar' }).click();
   }
 
   async informe2FA(codigo) {
-    await this.page.getByRole('textbox', { name: '000000' }).fill(codigo);
-    await this.page.getByRole('button', { name: 'Verificar' }).click();
+      await this.page.getByRole('textbox', { name: '000000' }).fill(codigo);
+      await this.page.getByRole('button', { name: 'Verificar' }).click();
   }
-
 }
